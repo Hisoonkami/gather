650da0fc15e8408f8208eafe9508da0f2f58de16
sjchen <chenshijia424@163.com>
2020-10-02 21:47:01 +0800

    配置修改

--- src/main/java/com/adev/gather/GatherApplication.java
+++ src/main/java/com/adev/gather/GatherApplication.java
@@ -11,7 +11,6 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @EnableEurekaClient
 @EnableJpaAuditing
 @EnableMongoAuditing
-@ComponentScan
 public class GatherApplication {
 
 	public static void main(String[] args) {
